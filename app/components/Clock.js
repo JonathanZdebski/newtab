7140ef92bbf62310daf8455142e54b3bdf7b75fe
JonathanZdebski <jonathanzdebski@yahoo.com>
2024-03-16 14:01:37 -0300

    Clock Adjusts

--- app/components/Clock.js
+++ app/components/Clock.js
@@ -1,41 +1,15 @@
-import { useState, useEffect } from "react";
-import styles from "./Clock.module.css"; // Certifique-se de que o caminho do arquivo CSS está correto
+import styles from "./newClock.module.css"; // Certifique-se de que o caminho do arquivo CSS está correto
 
 const Clock = () => {
-  const [time, setTime] = useState("");
-
-  useEffect(() => {
-    // Inicializa o estado com um valor padrão para evitar discrepâncias
-    setTime(
-      new Date().toLocaleTimeString("en-US", {
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-        hour12: false,
-        timeZone: "America/Sao_Paulo",
-      })
-    );
-
-    // Define um intervalo para atualizar o estado a cada segundo
-    const timer = setInterval(() => {
-      setTime(
-        new Date().toLocaleTimeString("en-US", {
-          hour: "2-digit",
-          minute: "2-digit",
-          second: "2-digit",
-          hour12: false,
-          timeZone: "America/Sao_Paulo",
-        })
-      );
-    }, 1000);
-
-    // Limpa o intervalo quando o componente é desmontado
-    return () => clearInterval(timer);
-  }, []);
-
   return (
     <div className={styles.clockContainer}>
-      <div>{time}</div>
+      <iframe
+        src="https://widgets.commoninja.com/iframe/ba47e24e-fbbe-4956-8d54-f325d0ce2b09"
+        width="100%"
+        height="100%"
+        frameBorder="0"
+        scrolling="no"
+      ></iframe>
     </div>
   );
 };
