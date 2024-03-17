83821c9fde38fe36fb7d47e963246c7a2e6d688f
JonathanZdebski <jonathanzdebski@yahoo.com>
2024-03-17 18:25:01 -0300

    Redeploy

--- app/components/BackgroundManager.js
+++ app/components/BackgroundManager.js
@@ -1,32 +1,40 @@
 import React, { useState, useEffect } from "react";
 
 const BackgroundManager = () => {
-  const [background, setBackground] = useState(
-    localStorage.getItem("background") || "bg1.jpg"
-  );
-  const [showImages, setShowImages] = useState(false); // Estado para controlar a visibilidade
+  const [background, setBackground] = useState("");
+  const [showImages, setShowImages] = useState(false);
 
   useEffect(() => {
-    if (background.startsWith("#")) {
-      document.body.style.backgroundColor = background;
-      document.body.style.backgroundImage = "none";
-    } else if (background === "Wallpaper Padrão") {
-      document.body.style.backgroundImage = `url(/default-wallpaper.jpg)`;
-      document.body.style.backgroundColor = "transparent";
-    } else if (background === "bg2.jpg") {
-      document.body.style.backgroundImage = `url(/bg2.jpg)`;
-      document.body.style.backgroundColor = "transparent";
-    } else if (background === "bg3.jpg") {
-      document.body.style.backgroundImage = `url(/bg3.jpg)`;
-      document.body.style.backgroundColor = "transparent";
-    } else {
-      document.body.style.backgroundImage = `url(${background})`;
-      document.body.style.backgroundColor = "transparent";
+    // Verifica se o código está sendo executado no lado do cliente
+    if (typeof window !== "undefined") {
+      const storedBackground = localStorage.getItem("background") || "bg1.jpg";
+      setBackground(storedBackground);
     }
-    localStorage.setItem("background", background);
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      localStorage.setItem("background", background);
+
+      if (background.startsWith("#")) {
+        document.body.style.backgroundColor = background;
+        document.body.style.backgroundImage = "none";
+      } else if (background === "Wallpaper Padrão") {
+        document.body.style.backgroundImage = `url(/default-wallpaper.jpg)`;
+        document.body.style.backgroundColor = "transparent";
+      } else if (background === "bg2.jpg") {
+        document.body.style.backgroundImage = `url(/bg2.jpg)`;
+        document.body.style.backgroundColor = "transparent";
+      } else if (background === "bg3.jpg") {
+        document.body.style.backgroundImage = `url(/bg3.jpg)`;
+        document.body.style.backgroundColor = "transparent";
+      } else {
+        document.body.style.backgroundImage = `url(${background})`;
+        document.body.style.backgroundColor = "transparent";
+      }
+    }
   }, [background]);
 
-  // Incluindo o wallpaper padrão e as cores na lista de opções
   const backgrounds = [
     "Wallpaper Padrão",
     "#FFDAB9",
@@ -42,7 +50,7 @@ const BackgroundManager = () => {
       <img
         src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='white' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-edit-2'%3E%3Cpath d='M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z'/%3E%3C/svg%3E"
         alt="Personalize"
-        style={{ cursor: "pointer", width: "34px", height: "54px" }} // Ajuste o tamanho conforme necessário
+        style={{ cursor: "pointer", width: "34px", height: "54px" }}
         onClick={() => setShowImages(!showImages)}
       />
 
@@ -71,7 +79,7 @@ const BackgroundManager = () => {
                 key={index}
                 style={{
                   display: "flex",
-                  flexDirection: "column", // Alinhando os itens em coluna
+                  flexDirection: "column",
                   alignItems: "center",
                   cursor: "pointer",
                 }}
@@ -79,9 +87,9 @@ const BackgroundManager = () => {
               >
                 <div
                   style={{
-                    width: "40px", // Aumentando o tamanho do quadrado
-                    height: "40px", // Aumentando o tamanho do quadrado
-                    marginRight: "10px", // Ajustando a posição à direita
+                    width: "40px",
+                    height: "40px",
+                    marginRight: "10px",
                     backgroundColor:
                       bg === "Wallpaper Padrão" ? "transparent" : bg,
                     backgroundImage:
@@ -89,18 +97,17 @@ const BackgroundManager = () => {
                         ? `url(https://images.pexels.com/photos/994605/pexels-photo-994605.jpeg)`
                         : bg === "bg2.jpg"
                         ? `url(https://m.media-amazon.com/images/I/71RGoav5XdL._SL1500_.jpg)`
-                        : bg === "bg3.jpg" // Ajustando a imagem de preview para a nova imagem
+                        : bg === "bg3.jpg"
                         ? `url(https://m.media-amazon.com/images/I/814VxtZJHFL._SL1500_.jpg)`
                         : "none",
-                    backgroundSize: "cover", // Ajustando o tamanho do background
-                    borderRadius: "50%", // Fazendo o círculo de preview
-                    marginLeft: "auto", // Posicionando à direita
+                    backgroundSize: "cover",
+                    borderRadius: "50%",
+                    marginLeft: "auto",
                   }}
                 ></div>
-                {/* Ocultando a escrita das cores */}
                 <span style={{ color: "white", visibility: "hidden" }}>
                   {bg}
-                </span>{" "}
+                </span>
               </li>
             ))}
           </ul>
